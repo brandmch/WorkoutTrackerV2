ccb48474941e8637c85bb0d208ba4975ff0f511d
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-02 19:31:38 -0500

    cosmetic Changes

--- components/createWorkoutInstance.js
+++ components/createWorkoutInstance.js
@@ -1,6 +1,6 @@
 import { Input, SearchBar, Text, ListItem } from "@rneui/base";
 import { useEffect, useState } from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, View, StyleSheet } from "react-native";
 import listOfTargets from "../data/utils/getListOfTargets";
 import listOfBodyParts from "../data/utils/getListOfBodyParts";
 import capitalize from "../utils/capitalize";
@@ -16,40 +16,50 @@ export default function CreateWorkoutInstance({
     : [...listOfTargets];
 
   return (
-    <ListItem.Accordion
-      content={
-        <ListItem.Content>
-          <ListItem.Title h4>
-            {capitalize(currWOobj.targets[ind])}
-          </ListItem.Title>
-        </ListItem.Content>
-      }
-      isExpanded={expanded}
-      onPress={() => setExpanded(!expanded)}
-      onLongPress={() => {
-        let tempArr = [...currWOobj.targets];
-        tempArr.splice(ind, 1);
-        setCurrWOobj({ ...currWOobj, targets: tempArr });
-      }}
-    >
-      <ScrollView>
-        {options.map((curr, index) => (
-          <ListItem
-            key={index}
-            bottomDivider
-            onPress={() => {
-              let tempArr = [...currWOobj.targets];
-              tempArr[ind] = options[index];
-              setCurrWOobj({ ...currWOobj, targets: [...tempArr] });
-              setExpanded(false);
-            }}
-          >
-            <ListItem.Content>
-              <ListItem.Title>{curr}</ListItem.Title>
-            </ListItem.Content>
-          </ListItem>
-        ))}
-      </ScrollView>
-    </ListItem.Accordion>
+    <View style={styles.view}>
+      <ListItem.Accordion
+        content={
+          <ListItem.Content>
+            <ListItem.Title h4>
+              {capitalize(currWOobj.targets[ind])}
+            </ListItem.Title>
+          </ListItem.Content>
+        }
+        isExpanded={expanded}
+        onPress={() => setExpanded(!expanded)}
+        onLongPress={() => {
+          let tempArr = [...currWOobj.targets];
+          tempArr.splice(ind, 1);
+          setCurrWOobj({ ...currWOobj, targets: tempArr });
+        }}
+      >
+        <ScrollView>
+          {options.map((curr, index) => (
+            <ListItem
+              key={index}
+              bottomDivider
+              onPress={() => {
+                let tempArr = [...currWOobj.targets];
+                tempArr[ind] = options[index];
+                setCurrWOobj({ ...currWOobj, targets: [...tempArr] });
+                setExpanded(false);
+              }}
+            >
+              <ListItem.Content>
+                <ListItem.Title>{curr}</ListItem.Title>
+              </ListItem.Content>
+            </ListItem>
+          ))}
+        </ScrollView>
+      </ListItem.Accordion>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  view: {
+    marginVertical: 1,
+    marginHorizontal: 10,
+    borderRadius: 50,
+  },
+});
