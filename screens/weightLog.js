04609351fc8fdb9391dd396e32922aaf0e459d12
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-03 13:10:37 -0500

    weightLog init

--- screens/weightLog.js
+++ screens/weightLog.js
@@ -1,5 +1,41 @@
-import { Text } from "@rneui/themed";
+import { useState } from "react";
+import { View } from "react-native";
+import { Text, FAB, Overlay, Input, Button } from "@rneui/themed";
 
 export default function WeightLog() {
-  return <Text>WeightLoss!</Text>;
+  const [overlayVisable, setOverlayVisable] = useState(false);
+  const [weights, setWeights] = useState([]);
+  const [input, setInput] = useState("");
+
+  return (
+    <View style={{ height: "100%" }}>
+      <Text>WeightLoss!</Text>
+      {weights.map((curr, ind) => (
+        <Text key={ind}>{curr}</Text>
+      ))}
+      <FAB
+        icon={{ name: "add", color: "white" }}
+        placement="right"
+        style={{ paddingRight: 10, paddingBottom: 20 }}
+        onPress={() => setOverlayVisable(true)}
+      />
+      <Overlay
+        isVisible={overlayVisable}
+        onBackdropPress={() => setOverlayVisable(false)}
+        overlayStyle={{ width: "60%" }}
+      >
+        <Input
+          placeholder="Weight  (lbs)"
+          onChangeText={(text) => setInput(text)}
+        />
+        <Button
+          title="Log"
+          onPress={() => {
+            setWeights([...weights, input]);
+            setOverlayVisable(false);
+          }}
+        />
+      </Overlay>
+    </View>
+  );
 }
